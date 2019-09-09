b2af572049a42664c81d8d5f8aee38796f284d01
Roshan Singh Rautela <54948078+RoNi1999@users.noreply.github.com>
2019-09-09 08:46:49 -0700

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int x,y,z;
+    printf("Enter two Number :");
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("Total=%d",z);
+    getch();
+
+}
---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter Two Numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("Sum is %d\n",a+b);    //Roni is here
+    return 0;
+}
